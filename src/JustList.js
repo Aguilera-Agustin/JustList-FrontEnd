c355c5557b1e9597d051def374c1f24cb2ceb25e
Agustin Aguilera <agustin.aguilera424@gmail.com>
2021-05-08 12:26:50 -0300

    complete redux config

--- src/JustList.js
+++ src/JustList.js
@@ -1,12 +1,14 @@
 import { CssBaseline } from '@material-ui/core'
 import React from 'react'
+import { Provider } from 'react-redux'
 import { AppRouter } from './routers/AppRouter'
+import {store} from './redux/store/store'
 
 export const JustList = () => {
     return (
-        <>
+        <Provider store={store}>
             <CssBaseline/>
             <AppRouter/>
-        </>
+        </Provider>
     )
 }
